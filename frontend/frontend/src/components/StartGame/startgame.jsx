4248601ce695429336327ba26f5c5aaae1eda1fa
verobragina <verobragina@ya.ru>
2022-07-13 17:20:33 +0300

    Fix Start and Login pages

--- frontend/frontend/src/components/StartGame/startgame.jsx
+++ frontend/frontend/src/components/StartGame/startgame.jsx
@@ -1,35 +1,33 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-//Images
-import pokeball from '../../Ảnh Pokemon Dự Trù/покебол-min.svg';
-import logo from '../../Ảnh Pokemon Dự Trù/логотип.svg';
-import footer from '../../Ảnh Pokemon Dự Trù/Vector_128.svg';
-import blackline from '../../Ảnh Pokemon Dự Trù/1_1.svg';
+// Images
+import Logo from '../../Ảnh Pokemon Dự Trù/логотип.svg';
+import Background from '../../assets/Bg-ball.svg';
+import MainBackground from '../../Ảnh Pokemon Dự Trù/фон общий-min.svg';
 
 const Startgame = () => {
 
-  const navigate=useNavigate();  
+  const navigate = useNavigate();
 
-  const startGame=()=>{
+  const startGame = () => {
     navigate("/login");
   }
 
   return (
     <div className="background-start">
-        {/*Put Image Background Here*/}
-        <img src={logo} className="logo-tokemon" alt=""/>
+      {/*Put Image Background Here*/ }
+      <img src={ Logo } className="logo-tokemon" alt=""/>
 
-        <img src={pokeball} className="pokeball-start" alt=""/>
+      <div className='backgrounds'>
+        <img src={ MainBackground } className="main-background" alt=""/>
+        <img src={ Background } className="background-blue" alt=""/>
+      </div>
 
-        <div className="start-game" onClick={startGame}>Start</div>
 
-        <div>
-          <img src={blackline} className="blackline-left" alt=""/>
-          <img src={footer} className="footer" alt=""/>
-          <img src={blackline} className="blackline-right" alt=""/>
-        </div>
-
+      <div className='start-button-container'>
+        <span className="start-game" onClick={ startGame }>start</span>
+      </div>
     </div>
   )
 }
