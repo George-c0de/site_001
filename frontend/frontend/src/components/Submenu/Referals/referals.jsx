d9b0d3f68c9c2cc644821e1319063d06cfe46c4c
verobragina <verobragina@ya.ru>
2022-07-13 19:24:05 +0300

    Fix Referals page

--- frontend/frontend/src/components/Submenu/Referals/referals.jsx
+++ frontend/frontend/src/components/Submenu/Referals/referals.jsx
@@ -51,91 +51,91 @@ const Referals = () => {
   return (
     <div className="homepage">
       <div className="main_container">
-        <nav className={onActive ? "navbar" : "navbar-new_active"}>
+        <nav className={ onActive ? "navbar" : "navbar-new_active" }>
           <FontAwesomeIcon
-            icon={faBars}
-            className={onActive ? "icon-menu" : "icon-new-menu_active"}
-            onClick={backHome}
+            icon={ faBars }
+            className={ onActive ? "icon-menu" : "icon-new-menu_active" }
+            onClick={ backHome }
           />
 
-          {openDropDown ? (
+          { openDropDown ? (
             <div className="container">
-                <div className="title-referal">Referral link</div>
-                <div className="link-invitation"></div>
+              <div className="title-referal">Referral link</div>
+              <div className="link-invitation"></div>
 
-                <div className="row" id="info-3-col">
+              <div className="row" id="info-3-col">
                 <div className="col-sm">
-                    <button className="row">1 line</button>
-                    Total person:
-                    <ul className="row">
-                        <li>140</li>
-                        <li>140</li>
-                        <li>140</li>
-                    </ul>
-                </div>
-                <div className="col-sm">
-                <button className="row">2 line</button>
-                    Profit received:
-                    <ul className="row">
-                        <li>240$</li>
-                        <li>240$</li>
-                        <li>240$</li>
-                    </ul>
+                  <button className="row">1 line</button>
+                  <span>Total person:</span>
+                  <ul className="row">
+                    <li>140</li>
+                    <li>140</li>
+                    <li>140</li>
+                  </ul>
                 </div>
                 <div className="col-sm">
-                <button className="row">3 line</button>
-                    Lost profits:
-                    <ul className="row">
-                        <li>246$</li>
-                        <li>246$</li>
-                        <li>246$</li>
-                    </ul>
+                  <button className="row">2 line</button>
+                  <span>Profit received:</span>
+                  <ul className="row">
+                    <li>240$</li>
+                    <li>240$</li>
+                    <li>240$</li>
+                  </ul>
                 </div>
+                <div className="col-sm">
+                  <button className="row">3 line</button>
+                  <span>Lost profits:</span>
+                  <ul className="row">
+                    <li>246$</li>
+                    <li>246$</li>
+                    <li>246$</li>
+                  </ul>
                 </div>
-          </div>
-          ) : null}
+              </div>
+            </div>
+          ) : null }
 
           <img
-            className={onActive ? "pokemon-banner" : "pokemon-banner-new_active"}
-            src={logo}
+            className={ onActive ? "pokemon-banner" : "pokemon-banner-new_active" }
+            src={ logo }
             alt=""
           />
 
           <img
-            src={human}
-            className={onActive ? "icon-user" : "icon-user-new_active"}
-            onClick={showNewMenu}
+            src={ human }
+            className={ onActive ? "icon-user" : "icon-user-new_active" }
+            onClick={ showNewMenu }
             alt=""
           />
-          {openNewDropDown ? (
+          { openNewDropDown ? (
             <ul className="user-info">
               <span className="user-id">User Id-</span>
 
-              <li>Balance: </li>
-              <li>Cards profit: </li>
-              <li>Referal profit: </li>
+              <li>Balance:</li>
+              <li>Cards profit:</li>
+              <li>Referal profit:</li>
               <li>Link for invitation</li>
               <div className="link-invitation"></div>
 
               <button className="yellow-btn">Deposit</button>
               <button className="yellow-btn">Withdraw</button>
 
-              <img className="satoshi" src={satoshi} alt="" />
+              <img className="satoshi" src={ satoshi } alt=""/>
             </ul>
-          ) : null}
+          ) : null }
 
           <button
-            className={onActive ? "white_btn_in" : "white_btn_in-new_active"}
-            onClick={handleLogout}
+            className={ onActive ? "white_btn_in" : "white_btn_in-new_active" }
+            onClick={ handleLogout }
           >
             Logout
           </button>
         </nav>
       </div>
-      {/* <img  src={pokeball} alt="" className="pokeball"/> */}
-      <div className={onActive ? "site-main" : "site-main-new_active"}>
+      {/* <img  src={pokeball} alt="" className="pokeball"/> */ }
+      <div className={ onActive ? "site-main" : "site-main-new_active" }>
         <img
-          src={britain}
+          src={ britain }
           className={
             onActive ? "english-icon-mainpage" : "english-icon-mainpage-new_active"
           }
@@ -143,7 +143,7 @@ const Referals = () => {
         />
 
         <img
-          src={support}
+          src={ support }
           className={
             onActive ? "support-icon-mainpage" : "support-icon-mainpage-new_active"
           }
