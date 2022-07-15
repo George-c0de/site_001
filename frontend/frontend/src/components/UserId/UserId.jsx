e008846ae1ea982a4644eed178282b2d6d4fdc58
verobragina <verobragina@ya.ru>
2022-07-15 11:47:12 +0300

    Add link copying

--- frontend/frontend/src/components/UserId/UserId.jsx
+++ frontend/frontend/src/components/UserId/UserId.jsx
@@ -1,23 +1,44 @@
+import React, { useRef } from 'react';
+
+// Images
 import satoshi from '../../Ảnh Pokemon Dự Trù/123133.svg';
+
+// Icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCopy } from '@fortawesome/free-solid-svg-icons';
+
 import './UserId.css';
 
-export const UserId = ({ note }) => (
-  <div className="user-info-container">
-    <span className="user-id">User Id-{ note?.id }</span>
-    <div className='user-info-wrapper'>
-      <ul className="user-info">
-        <li>Balance:{ note?.money }</li>
-        <li>Cards profit: { note?.money }</li>
-        <li>Referal profit: { note?.line_1 + note?.line_2 + note?.line_3 }</li>
-        <li>Link for invitation { note?.referral_link }</li>
-        <div className="link-invitation">{ note?.referral_link }</div>
-        <div className='user-info-buttons'>
-          <button className="yellow-btn">Deposit</button>
-          <button className="yellow-btn">Withdraw</button>
-        </div>
-      </ul>
+export const UserId = ({ note }) => {
+  const linkRef = useRef();
 
-      <img className="satoshi" src={ satoshi } alt=""/>
+  const handleCopy = () => {
+    navigator.clipboard.writeText(linkRef.current?.innerText);
+  }
+
+  return (
+    <div className="user-info-container">
+      <span className="user-id">User Id-{ note?.id }</span>
+      <div className='user-info-wrapper'>
+        <ul className="user-info">
+          <li>Balance:{ note?.money }</li>
+          <li>Cards profit: { note?.money }</li>
+          <li>Referal profit: { note?.line_1 + note?.line_2 + note?.line_3 }</li>
+          <li>Link for invitation { note?.referral_link }</li>
+
+          <span className="link-invitation" ref={linkRef}>
+            { note?.referral_link }
+            <FontAwesomeIcon icon={ faCopy } className='copy-icon' onClick={ handleCopy }/>
+          </span>
+
+          <div className='user-info-buttons'>
+            <button className="yellow-btn">Deposit</button>
+            <button className="yellow-btn">Withdraw</button>
+          </div>
+        </ul>
+
+        <img className="satoshi" src={ satoshi } alt=""/>
+      </div>
     </div>
-  </div>
-)
+  )
+}
