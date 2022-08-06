a33b53beaabcf04bc506a240bd402656ab1fadcb
Arkhipov <72arkhipov@mail.ru>
2022-08-06 17:34:38 +0500

    create ResetPass

---
+++ frontend/frontend/src/components/ResetPass/Resetpass.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from '../../Ảnh Pokemon Dự Trù/логотип.svg'
+const Resetpass = () => {
+	return (
+		<div>
+			<div className='navbar-container'>
+				<nav className='navbar'>
+					<img src={logo} className='logo-tokemon' alt='' />
+				</nav>
+			</div>
+		</div>
+	)
+}
+
+export default Resetpass
