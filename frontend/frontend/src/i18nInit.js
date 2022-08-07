9486e076d6a8445d367868b2027ef696f43dc169
Arkhipov <72arkhipov@mail.ru>
2022-08-07 22:24:12 +0500

    bug fix with language display

--- frontend/frontend/src/i18nInit.js
+++ frontend/frontend/src/i18nInit.js
@@ -1,21 +1,22 @@
-import {addLocale, useLocale as localize} from 'ttag';
-import * as cookie from './cookie';
+import { addLocale, useLocale as localize } from 'ttag'
+import * as cookie from './cookie'
 
-const LOCALE_COOKIE = '__locale';
+const LOCALE_COOKIE = '__locale'
 
 function getLocale() {
-    return cookie.get(LOCALE_COOKIE) || 'en';
+	return cookie.get(LOCALE_COOKIE) || 'en'
 }
 
 export function saveLocale(locale) {
-    cookie.set(LOCALE_COOKIE, locale);
+	cookie.set(LOCALE_COOKIE, locale)
 }
 
 // setup
-const locale = getLocale();
+let locale = getLocale()
 
 if (locale !== 'en') {
-    const translationsObj = require(`../i18n/${locale}.po.json`)
-    addLocale(locale, translationsObj);
-    localize(locale);
+	if (locale == 'undefined') locale = 'ru'
+	const translationsObj = require(`../i18n/${locale}.po.json`)
+	addLocale(locale, translationsObj)
+	localize(locale)
 }
