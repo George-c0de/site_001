ceee66b0b73b4ff854f902a5cb98422685d488ab
Arkhipov <72arkhipov@mail.ru>
2022-08-10 20:04:46 +0500

    fix

--- frontend/frontend/src/components/Header/Header.jsx
+++ frontend/frontend/src/components/Header/Header.jsx
@@ -55,13 +55,30 @@ const Header = () => {
 			setOpenStatistics(false)
 		}
 	}
-
+	
 	React.useEffect(() => {
+		fetchLink()
 		document.addEventListener('click', handleClickOutside, true)
 		return () => {
 			document.removeEventListener('click', handleClickOutside, true)
 		}
 	}, [])
+
+	const [get_link_tg, setget_link_tg] = React.useState({
+		link_tg: 'https://t.me/Tokemon_game_Bot',
+	})
+
+	async function fetchLink() {
+		try {
+			const response = await axios.get('/api/get_link_tg')
+			setget_link_tg(response.data.link_tg)
+		} catch (e) {
+			if (e.response.status !== 200) {
+				let data_ = 'https://t.me/Tokemon_game_Bot'
+				setget_link_tg(data_)
+			}
+		}
+	}
 
 	return (
 		<>
@@ -114,11 +131,7 @@ const Header = () => {
 							</li>
 						</ul>
 						<div className='main__blocks'>
-							<a
-								className='main__block'
-								href='https://t.me/Tokemon_game_Bot'
-								target='__blank'
-							>
+							<a className='main__block' href={get_link_tg} target='__blank'>
 								<img src={Telegram} alt='' />
 								<p className='main__text'>{t`USE TELEGRAM BOT`}</p>
 							</a>
