aba58ab01341c2b2ce6710af534de34319c92bcb
Arkhipov <72arkhipov@mail.ru>
2022-08-10 15:31:59 +0500

    fix responsive

--- frontend/frontend/src/components/StartGame/startgame.jsx
+++ frontend/frontend/src/components/StartGame/startgame.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
-import MainBg from '../../assets/MainBg.png'
+import MainBg from '../../assets/MainBg.jpg'
 import { Lang } from '../Lang/Lang'
+import Pokebol from '../../assets/pockebol.png'
 import Logo from '../../assets/header/logo.svg'
 let data2
 try {
@@ -26,7 +27,7 @@ const Startgame = () => {
 	return (
 		<div className='background-start'>
 			<img src={Logo} className='logo-tokemon' alt='' />
-			<p href='##' onClick={startGame} className='start-link'></p>
+			<img onClick={startGame} src={Pokebol} alt=""  className='logo-pokebol'/>
 			<img src={MainBg} alt='' className='main-bg' />
 			<Lang isActive={onActive} />
 		</div>
